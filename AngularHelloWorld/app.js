1128892ad91a37bf62f6a2639ef96e6c6e22b977
beewhy5 <34488305+beewhy5@users.noreply.github.com>
2018-01-09 13:54:22 -0500

    Added files for initial check in

---
+++ AngularHelloWorld/app.js
@@ -0,0 +1,36 @@
+﻿var app = angular.module('myApp', []); // registers noteApp with Angular
+
+app.directive('myTodo',
+    function() {
+        return {
+            restrict: 'EA',
+            templateUrl: 'todo.tpl.html',
+            scope: {
+                list: '=',
+                title: '@'
+            }
+        };
+
+    });
+
+app.controller('main', function ($scope) {
+    $scope.todo = [
+        { name: 'BILLS WIN!', completed: true },
+        { name: 'Ravens lose', completed: false },
+        { name: 'Titans lose', completed: true },
+        { name: 'Chargers lose', completed: true }
+    ];
+
+});
+
+app.filter('completed', function () {
+    return function (input) {
+        var out = [];
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].completed) {
+                out.push(input[i]);
+            }
+        }
+        return out;
+    }
+});
